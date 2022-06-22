6cb5f14e5730d3ed77966d47bdb562ebe437fe6e
justincj2001 <74088655+justincj2001@users.noreply.github.com>
2022-06-22 07:09:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-import process from 'process'
-import minimist from 'minimist'
-import { Web3Storage, getFilesFromPath } from 'web3.storage'
+const express = require('express')
+const app = express()
+const port = 3000
 
-async function main () {
-  const args = minimist(process.argv.slice(2))
-  const token = args.token
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-  if (!token) {
-    return console.error('A token is needed. You can create one on https://web3.storage')
-  }
-
-  if (args._.length < 1) {
-    return console.error('Please supply the path to a file or directory')
-  }
-
-  const storage = new Web3Storage({ token })
-  const files = []
-
-  for (const path of args._) {
-    const pathFiles = await getFilesFromPath(path)
-    files.push(...pathFiles)
-  }
-
-  console.log(`Uploading ${files.length} files`)
-  const cid = await storage.put(files)
-  console.log('Content added with CID:', cid)
-}
-
-main()
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
