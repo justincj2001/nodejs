585aa1fa7c83f2ac2757f20d8f674a13dc7bf9d6
justincj2001 <74088655+justincj2001@users.noreply.github.com>
2022-06-23 06:05:30 +0530

    Update request.js

--- request.js
+++ request.js
@@ -1,4 +1,5 @@
 const http = require('https');
+const url = require('url');
 const port = process.env.PORT || 3000
 fs = require('fs');
 f= require('./put-files');
@@ -6,16 +7,14 @@ require('process');
 minimist= require('minimist');
 Web3Storage =require('web3.storage').Web3Storage;
 getFilesFromPath= require('web3.storage').getFilesFromPath;
+global.a="not found"
 global.b=false;
-global.a="10";
 var cors = require('cors')
 var express = require('express');
 var app = express();
 app.use(cors())
 
-
-
-async function upload() {
+async function upload(rando) {
   const args = minimist(process.argv.slice(2))
   const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDI5MTQzNzEzMjc0RUNmZDJGQzVFZjVjZmI5NDU1NzY5YjY5RTE2OUYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTU2NDU4MDEwMDEsIm5hbWUiOiJzb2NpYWwgbWVkaWEifQ.9iwWem7JCQQiD_GCuq8VMucBwj0EBWfN3ZZ7ZiQIQ90"
 
@@ -36,38 +35,42 @@ async function upload() {
   const cid = await storage.put(files)
   console.log('Content added with CID:', cid)
   global.b=true;
-  global.a="https://dweb.link/ipfs/"+cid+"/"+global.ran+".jpg";
+  global.a="https://dweb.link/ipfs/"+cid+"/"+rando+".jpg";
   return;
 }
 
-setTimeout(()=>{let server=app.get('/site', function (req, res) {
-  var randomstring = Math.random().toString(36).slice(-8);
-  global.ran=randomstring;
 
-  const file = fs.createWriteStream(randomstring+".jpg");
-  const str1=req.query.url;
-  console.log(str1);
-  //this is link for the image
-const request = http.get(str1, function(response) {
-   response.pipe(file);
 
-   // after download completed close filestream
-   file.on("finish", () => {
-       file.close();
-       upload();
-       
-   });
-});
-if(!global.b){
-  res.status(404).send("Oh uh, wait for file to upload  status code: "+res.statusCode);
-    return;
+http
+  .createServer(function (req, res) {
+    const queryObject = url.parse(req.url, true).query;
 
-}
-  res.header("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Accept");
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(global.a);
-  res.end();
 
+    var randomstring = Math.random().toString(36).slice(-8);
+    global.ran=randomstring;
   
-}).listen(port);},1000);
+    const file = fs.createWriteStream(global.ran+".jpg");
+   
+    //this is link for the image
+  const request = http.get(queryObject.url, function(response) {
+     response.pipe(file);
+  
+     // after download completed close filestream
+     file.on("finish", () => {
+         file.close();
+         upload(global.ran);
+         
+     });
+  });
 
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    console.log(queryObject.url);
+    
+    if(global.b)
+    {
+      res.write(global.a);
+      res.end();
+    }
+
+  })
+  .listen(port);
