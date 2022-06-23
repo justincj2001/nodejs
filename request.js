ff1ff8430cc16862b5d4a768225eab9929740e63
justincj2001 <74088655+justincj2001@users.noreply.github.com>
2022-06-23 06:12:15 +0530

    Update request.js

--- request.js
+++ request.js
@@ -1,5 +1,4 @@
-const http = require('https');
-const url = require('url');
+const http = require('http');
 const port = process.env.PORT || 3000
 fs = require('fs');
 f= require('./put-files');
@@ -7,12 +6,14 @@ require('process');
 minimist= require('minimist');
 Web3Storage =require('web3.storage').Web3Storage;
 getFilesFromPath= require('web3.storage').getFilesFromPath;
-global.a="not found"
 global.b=false;
+global.a="10";
 var cors = require('cors')
 var express = require('express');
 var app = express();
 app.use(cors())
+
+
 
 async function upload(rando) {
   const args = minimist(process.argv.slice(2))
@@ -28,7 +29,7 @@ async function upload(rando) {
 
   storage = new Web3Storage({ token })
   const files = []
-  const pathFiles = await getFilesFromPath(global.ran+".jpg")
+  const pathFiles = await getFilesFromPath(rando+".jpg")
   files.push(...pathFiles)
 
   console.log(`Uploading ${files.length} files`)
@@ -39,38 +40,33 @@ async function upload(rando) {
   return;
 }
 
+setTimeout(()=>{let server=app.get('/site', function (req, res) {
+  var randomstring = Math.random().toString(36).slice(-8);
+  global.ran=randomstring;
 
+  const file = fs.createWriteStream(randomstring+".jpg");
+  const str1=req.query.url;
+  console.log(str1);
+  //this is link for the image
+const request = http.get(str1, function(response) {
+   response.pipe(file);
 
-http
-  .createServer(function (req, res) {
-    const queryObject = url.parse(req.url, true).query;
+   // after download completed close filestream
+   file.on("finish", () => {
+       file.close();
+       upload(randomstring);
+       
+   });
+});
+if(!global.b){
+  res.status(404).send("Oh uh, wait for file to upload  status code: "+res.statusCode);
+    return;
 
+}
+  res.header("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Accept");
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write(global.a);
+  res.end();
 
-    var randomstring = Math.random().toString(36).slice(-8);
-    global.ran=randomstring;
-  
-    const file = fs.createWriteStream(global.ran+".jpg");
-   
-    //this is link for the image
-  const request = http.get(queryObject.url, function(response) {
-     response.pipe(file);
-  
-     // after download completed close filestream
-     file.on("finish", () => {
-         file.close();
-         upload(global.ran);
-         
-     });
-  });
+}).listen(port);},1000);
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    console.log(queryObject.url);
-    
-    if(global.b)
-    {
-      res.write(global.a);
-      res.end();
-    }
-
-  })
-  .listen(port);
